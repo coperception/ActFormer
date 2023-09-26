ea38e1e2c6c917564701f81f100aac8853dc65a7
huangsuozhi <1511508226@qq.com>
2023-09-26 23:48:54 +0800

    del

--- projects/mmdet3d_plugin/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .nuscenes_dataset import CustomNuScenesDataset
-from .v2x_sim_dataset import CustomV2XSIMDataset
-from .builder import custom_build_dataset
-
-__all__ = [
-    'CustomNuScenesDataset','CustomV2XSIMDataset'
-]
